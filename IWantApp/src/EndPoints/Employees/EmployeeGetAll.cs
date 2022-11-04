b1142cb8eec9dce3d6b9938e3bd90e2619114184
Reinan Guilherme de Oliveira Neto <reinan.neto@grupofasitec.local>
2022-11-04 16:54:59 -0300

    Iniciando Dapper

--- IWantApp/src/EndPoints/Employees/EmployeeGetAll.cs
+++ IWantApp/src/EndPoints/Employees/EmployeeGetAll.cs
@@ -1,7 +1,5 @@
-﻿using IWantApp.Domain.Products;
-using IWantApp.Infra.Data;
-using Microsoft.AspNetCore.Identity;
-using System.Security.Claims;
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
 
 namespace IWantApp.EndPoints.Employees
 {
@@ -11,25 +9,15 @@ namespace IWantApp.EndPoints.Employees
         public static string[] Methoods => new string[] { HttpMethod.Get.ToString() };
         public static Delegate Handle => Action;
 
-        //para salvar o usuario é necessario adicionar o UserManager<IdentityUser>
-        //paginacao
-        public static IResult Action(int page, int rows, UserManager<IdentityUser> userManager)
+        public static IResult Action(int page, int rows, IConfiguration configuration)
         {
-            var users = userManager.Users.Skip((page - 1) * rows).Take(rows).ToList();
+            var db = new SqlConnection(configuration["ConnectionStrings:IWantDb"]);
 
-            //resgatando a lista de claims
-            var employees = new List<EmployeeResponse>();
-            foreach (var user in users)
-            {
-                var claims = userManager.GetClaimsAsync(user).Result;
-                var claimName = claims.FirstOrDefault(c => c.Type == "Name");
-                var userName = claimName != null ? claimName.Value : string.Empty;
-                employees.Add(new EmployeeResponse(user.Email, userName));
-            }
+            string script = @"select Email, ClaimValue
+                                from AspNetUsers u 
+                                inner join AspNetUserClaims c on u.id = c.UserId and claimtype = 'Name'";
 
-            var employee = users.Select(u => new EmployeeResponse ( u.Email, "Name" ));
-
-            return Results.Ok(employee);
+            var employees = db.Query<EmployeeRequest>(script);
         }
     }
 }
