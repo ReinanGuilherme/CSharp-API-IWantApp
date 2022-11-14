3f4c67a5c8ec35911fe1f033b143363c794e406e
Reinan Guilherme de Oliveira Neto <reinan.neto@grupofasitec.local>
2022-11-14 10:35:26 -0300

    add jwt e auth

---
+++ IWantApp/src/EndPoints/Security/TokenPost.cs
@@ -0,0 +1,51 @@
+﻿using IWantApp.EndPoints.Employees;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace IWantApp.EndPoints.Security
+{
+    public class TokenPost
+    {
+        public static string Template => "/token";
+        public static string[] Methoods => new string[] { HttpMethod.Post.ToString() };
+        public static Delegate Handle => Action;
+
+        //é necessario adicionar o UserManager<IdentityUser>
+        public static IResult Action(LoginRequest loginRequest, UserManager<IdentityUser> userManager)
+        {
+            var user = userManager.FindByEmailAsync(loginRequest.Email).Result;
+            if(user == null)
+            {
+                return Results.BadRequest();
+            }
+            if(!userManager.CheckPasswordAsync(user, loginRequest.Password).Result)
+            {
+                return Results.BadRequest();
+            }
+
+            //gerando o token
+            var key = Encoding.ASCII.GetBytes("A@ferwfQQSDXCCer34");
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Email, loginRequest.Email),
+                }),
+                SigningCredentials = 
+                    new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
+                    Audience = "IWantApp",
+                    Issuer = "Issuer"
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return Results.Ok(new
+            {
+                token = tokenHandler.WriteToken(token)
+            });
+        }
+    }
+}
--- IWantApp/src/Program.cs
+++ IWantApp/src/Program.cs
@@ -1,8 +1,12 @@
 using IWantApp.EndPoints.Categories;
 using IWantApp.EndPoints.Employees;
+using IWantApp.EndPoints.Security;
 using IWantApp.Infra.Data;
 using IWantApp.Infra.Data.Querys;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSqlServer<ApplicationDbContext>(builder.Configuration["ConnectionStrings:IWantDb"]);
@@ -19,6 +23,26 @@ builder.Services.AddIdentity<IdentityUser, IdentityRole>(
     //add o context do banco de dados.
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
+//add JWT como serviço
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateActor = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["JwtBearerTokenSettings:Issuer"],
+        ValidAudience = builder.Configuration["JwtBearerTokenSettings:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["SecretKey"]))
+    };
+});
+
 //add funcao como serviço
 builder.Services.AddScoped<QueryAllUsersWithClaimName>();
 
@@ -28,6 +52,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+app.UseAuthorization();
+app.UseAuthentication();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -43,5 +69,6 @@ app.MapMethods(CategoryGetAll.Template, CategoryGetAll.Methoods, CategoryGetAll.
 app.MapMethods(CategoryPut.Template, CategoryPut.Methoods, CategoryPut.Handle);
 app.MapMethods(EmployeePost.Template, EmployeePost.Methoods, EmployeePost.Handle);
 app.MapMethods(EmployeeGetAll.Template, EmployeeGetAll.Methoods, EmployeeGetAll.Handle);
+app.MapMethods(TokenPost.Template, TokenPost.Methoods, TokenPost.Handle);
 
 app.Run();
